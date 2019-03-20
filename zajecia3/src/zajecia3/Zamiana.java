9e424f7d45acd9642600a25c0bec6d08f3180d1a
2mdjshfz <michal.peta@s.apsl.edu.pl>
2019-03-20 18:06:52 +0100

    final version

--- zajecia3/src/zajecia3/Zamiana.java
+++ zajecia3/src/zajecia3/Zamiana.java
@@ -1,5 +1,81 @@
 package zajecia3;
 
 public class Zamiana {
+	public static StringBuffer LetterReplace(StringBuffer a, String b, String c)
+	{
+		StringBuffer changed = a;
+		int pom = 0;
+		while(0 < changed.indexOf(b,0)) {
+			pom = changed.indexOf(b,0);
+			changed.replace(pom, pom + 1, c);
+		}
+		return changed;
+	}
+	public static void Zmien(String  a) {
+		StringBuffer text = new StringBuffer(a.toLowerCase());
+		int i = 0, pom = 0;
+		boolean sprawdz = false;
+		char[] samogloska = {'a', 'e', 'y', 'u', 'i', 'o'};
+		char[] spolgloska = {'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'r', 's', 't', 'w', 'y', 'z'};
 
+		text = LetterReplace(text, "h", "");
+		pom = 0;
+		for(i = 0; i < text.length(); i++)
+		{
+			sprawdz = false;
+			for(int j = 0; j < samogloska.length; j++)
+			{
+				if(text.charAt(i) == samogloska[j]) {
+					sprawdz = true;
+				}
+			}
+			
+			if(sprawdz && pom>0) {
+				text.replace(i, i+1, " ");
+			}
+			
+			if(sprawdz) {
+				pom++;
+			} else {
+				pom = 0;
+			}
+		}
+		text = LetterReplace(text, " ", "");
+		pom = 0;
+		for(i = 0;i < text.length(); i++)
+		{
+			sprawdz = false;
+			for(int j = 0; j < spolgloska.length; j++)
+			{
+				if(text.charAt(i) == spolgloska[j]) {
+					sprawdz = true;
+				}
+			}
+			if(sprawdz && pom > 0) {
+				text.replace(i, i+1, " ");
+			}
+			if(sprawdz) {
+				pom++;
+			} else {
+				pom = 0;
+			}
+		}
+		
+		text = LetterReplace(text, " ", "");
+		text = LetterReplace(text, "i", "e");
+		text = LetterReplace(text, "y", "e");
+		text = LetterReplace(text, "p", "b");
+		text = LetterReplace(text, "k", "c");
+		text = LetterReplace(text, "s", "c");
+		text = LetterReplace(text, "t", "d");
+		text = LetterReplace(text, "j", "g");
+		text = LetterReplace(text, "n", "m");
+		text = LetterReplace(text, "w", "v");
+
+		System.out.println(text);
+}
+	public static void main(String[] args) {
+		Zmien("Kowalski");
+		Zmien("Sierputowski");
+	}
 }
